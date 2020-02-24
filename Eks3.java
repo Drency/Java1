a0102b3bade76f73dd69fcb022fa01378f5c2d5e
Dency <martin.knudsen.sand@gmail.com>
2020-02-24 16:38:45 +0100

    fikser eks 3

--- Eks3.java
+++ Eks3.java
@@ -5,9 +5,9 @@ import static javax.swing.JOptionPane.*;
 
 public class Eks3{
     public static void main(String[] args) throws Exception{
-        String filnavn = showInputDialog("Skriv inn klasse navn: ");
+        String filnavn = showInputDialog("Skriv inn klasse navn, Eks - klasse3a: ");
 
-        File fil = new File(filnavn);
+        File fil = new File(filnavn + ".txt");
         Scanner leser = new Scanner(fil);
 
         List<Integer> liste = new ArrayList<Integer>();
@@ -18,6 +18,7 @@ public class Eks3{
             }
         }
 
+        leser.close();
         //Regner ut gjennomsnitt
         int telle = 0;
         int sum = 0;
@@ -29,8 +30,11 @@ public class Eks3{
 
         int gjennomsnitt = sum/telle;
 
+        //Finner høyeste og laveste student for og legger alle over og under gjennomsnittet i egen liste.
         List<Integer> underAvg = new ArrayList<Integer>();
         List<Integer> overAvg = new ArrayList<Integer>();
+        int tempHoy = 0;
+        int tempLav = 0;
 
         for(int i = 0; i< liste.size(); i++) {
             if(liste.get(i) < gjennomsnitt){
@@ -38,7 +42,41 @@ public class Eks3{
             } else if(liste.get(i) > gjennomsnitt){
                 overAvg.add(liste.get(i));
             }
+            if(i > tempHoy)
+                tempHoy = i;
+            else if(i < tempLav) 
+                tempLav = i;
         }
 
+        //Finner gjennomsnitt av de under gjennomsnitt
+        int sumUnder = 0;
+        for(int d : underAvg){
+            sumUnder+= d;
+        }
+
+        int avgUnder = sumUnder/underAvg.size();
+
+
+        //Finner gjennomsnitt av de som er over gjennomsnitt
+        int sumOver = 0;
+        
+        for(int x : overAvg){
+            sumOver += x;
+        }
+
+        int avgOver = sumOver/overAvg.size();
+        
+
+        PrintWriter skriver = new PrintWriter("res"+filnavn);
+
+        String ut = tempLav + " -  Minste Høyde" + "\n"
+                  + avgUnder + " -  Snitt Lave" + "\n"
+                  + gjennomsnitt + " -  Klassesnitt" + "\n"
+                  + avgOver + " - Snitt Høye" + "\n"
+                  + tempHoy + " - Største Høyde";
+
+        skriver.println(ut);
+
+        skriver.close();
     }
 }
